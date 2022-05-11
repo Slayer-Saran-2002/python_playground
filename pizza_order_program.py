5f6cd447551673be65db93d0d898cedf92f9f582
Saranjeet Mondal <76931124+Slayer-Saran-2002@users.noreply.github.com>
2022-05-11 13:38:36 +0530

    string BUG fixed

--- pizza_order_program.py
+++ pizza_order_program.py
@@ -13,19 +13,19 @@ add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 
 #Write your code below this line 👇
-if size =="S":
-    bill = 15
-    if add_pepperoni=="Y":
-        bill+=2
-elif size =="M":
-    bill = 20
-    if add_pepperoni=="Y":
-        bill+=3
-elif size =="L":
-    bill = 25
-    if add_pepperoni=="Y":
-        bill+=3
+if size.upper() =="S":
+    bill = 150
+    if add_pepperoni.upper()=="Y":
+        bill+=20
+elif size.upper() =="M":
+    bill = 200
+    if add_pepperoni.upper()=="Y":
+        bill+=30
+elif size.upper() =="L":
+    bill = 250
+    if add_pepperoni.upper()=="Y":
+        bill+=30
 
-if extra_cheese=="Y":
-    bill+=1
-printy(f"Your final bill is: ${bill}.")
+if extra_cheese.upper()=="Y":
+    bill+=10
+printy(f"Your final bill is: ₹ {bill}.")
