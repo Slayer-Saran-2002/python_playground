2eeaacbc359bd64112675e08598e638f106fd7bf
Saranjeet Mondal <76931124+Slayer-Saran-2002@users.noreply.github.com>
2022-05-20 00:30:14 +0530

    Delete Banker_roulett.py

--- Banker_roulett.py
+++
@@ -1,8 +0,0 @@
-import random
-# test_seed = int(input("Create a seed number: "))
-# random.seed(test_seed)
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(",")
-
-num = random.randint(0,len(names)-1)
-print(names[num],"is going to buy the meal today!")
